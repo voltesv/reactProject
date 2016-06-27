00d3b87977bb169763f3ec737e0d695cf384a544
Steve Armstrong <voltesv619@outlook.com>
2016-06-27 13:18:59 -0400

    Added gulp task(lint)

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 
 const gulp = require('gulp');
 const semver = require('semver');
+const jshint = require('gulp-jshint');
 
 //check node version task
 gulp.task('version', function (done) {
@@ -23,8 +24,23 @@ gulp.task('version', function (done) {
     }
     done();
 });
+
+//Check for code style errors using Lint
+gulp.task('lint', function () {
+    console.log('Linting JavaScript: ');
+    return gulp.src(['gulpfile.js','src/**/*.js'])
+    .pipe(jshint({
+            esversion: 6,
+            node: true,
+            browser: true,
+            globals: []
+        }))
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
 //default task
-gulp.task('default', gulp.series('version', function (done) {
+gulp.task('default', gulp.series(gulp.parallel('version','lint'), function (done) {
     console.log('BUILD OK');
     done();
 }));
