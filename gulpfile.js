2db25b242dcf5f488757d644d4280ddaeb77f265
Steve Armstrong <voltesv619@outlook.com>
2016-06-27 11:49:01 -0400

    added gulp task(version)

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,28 @@
  */
 
 const gulp = require('gulp');
+const semver = require('semver');
 
+//check node version task
+gulp.task('version', function (done) {
+    console.log("Checking node version:");
+
+    const actualVersion = process.version;
+    const packageJson = require('./package.json');
+    const expectedVersion = packageJson.engines.node;
+
+
+    if (semver.gt(expectedVersion,actualVersion)) {
+        console.log('Incorrect node version. Expected ' +
+        expectedVersion + '. Actual: ' + actualVersion);
+        process.exit(1);
+    } else {
+        console.log('Node Version:' + actualVersion);
+    }
+    done();
+});
 //default task
-gulp.task('default', function (done) {
+gulp.task('default', gulp.series('version', function (done) {
     console.log('BUILD OK');
     done();
 });
