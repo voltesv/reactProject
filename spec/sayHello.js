1f7067e88a437f86728b62dd52f3bbcc71d73730
Steve Armstrong <voltesv619@outlook.com>
2016-06-27 15:18:05 -0400

    installed gulp-jasmine

---
+++ spec/sayHello.js
@@ -0,0 +1,7 @@
+/**
+ * Created by n0212893 on 6/27/2016.
+ */
+exports.greet = function greet(name) {
+
+    return 'Hello, '  + name;
+}
---
+++ spec/sayHelloSpec.js
@@ -0,0 +1,19 @@
+/**
+ * Created by n0212893 on 6/27/2016.
+ */
+var sayHello = require('./sayHello.js');
+var name = "World";
+
+describe('Greet', function() {
+    it('concats Hello and a name', function () {
+        var actual = sayHello.greet(name);
+        var expected = 'Hello, World';
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe('Noname', function () {
+    it('checks for a valid/non-blank name', function () {
+        expect(name).toBeDefined();
+    });
+});
